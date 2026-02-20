fd3adcf5b0091e8ec2792557d83e85fad46ad973
Althep <pnr1367@gmail.com>
2026-02-20 21:54:51 +0900

    aa

---
+++ Assets/Scripts/Items/ConsumableOptions/ModifierActions/Mutate.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Mutate : ModifierAction
+{
+    public override void Reset()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetValueToString()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Items/ConsumableOptions/ModifierActions/ScrollEffects.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class ScrollEffects : ModifierAction
+{
+    public override void Reset()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetValueToString()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+public class TurnSkip : ModifierAction
+{
+    public override void Reset()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetValueToString()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+public class Identify : ScrollEffects
+{
+
+}
+
+public class UnEquipable : ScrollEffects
+{
+
+}
+
+public class Purification : ScrollEffects
+{
+
+}
+public class Tellevision : ScrollEffects
+{
+
+}
+public class ItemReinforce: ScrollEffects
+{
+
+}
+
+public class Blink : ScrollEffects
+{
+
+}
+
+
+public class Telleport : ScrollEffects
+{
+
+}
+
+public class DamageRange : ScrollEffects
+{
+
+}
+
